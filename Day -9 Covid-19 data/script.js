3cf0032970e52282ae290b6a2f897542859379d9
salonifojdar27 <salonifojdar27@gmail.com>
2025-04-27 10:22:10 +0530

    Add files via upload

---
+++ Day -9 Covid-19 data/script.js
@@ -0,0 +1,31 @@
+
+async function covidData()
+{
+    try{
+      const response = await fetch('https://api.rootnet.in/covid19-in/stats/latest');
+      const data =  await response.json();
+      const regions = data.data.regional;
+
+      const tableBody = document.getElementById('covidTable');
+      regions.forEach((region ,index)=>{
+        const totalCases = region.confirmedCasesIndian + region.confirmedCasesForeign;
+        const row = `
+        <tr>
+          <td>${index + 1}</td>
+          <td>${region.loc}</td>
+          <td>${region.confirmedCasesIndian}</td>
+          <td>${region.confirmedCasesForeign}</td>
+          <td>${region.discharged}</td>
+          <td>${region.deaths}</td>
+          <td>${totalCases}</td>
+        </tr>
+      `;
+      tableBody.innerHTML += row;
+    });
+  } catch (error) {
+    console.error('Error fetching COVID data:', error);
+  }
+}
+
+covidData();
+    
